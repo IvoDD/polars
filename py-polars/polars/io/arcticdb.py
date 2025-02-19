89d599b0242c315d0a029889183e57f8b5ebba01
Ivo Dilov <iddilov@gmail.com>
2025-02-19 14:58:30 +0200

    TODO: Work in progress predicate pushdown for adb

--- py-polars/polars/io/arcticdb.py
+++ py-polars/polars/io/arcticdb.py
@@ -24,6 +24,9 @@ import polars._reexport as pl
 from polars._utils.convert import to_py_date, to_py_datetime
 from polars.dependencies import arcticdb
 
+# TODO: Better imports
+from arcticdb.version_store.processing import ExpressionNode
+
 if TYPE_CHECKING:
     from datetime import date, datetime
 
@@ -94,23 +97,150 @@ def _scan_pyarrow_dataset_impl(
     from arcticdb import OutputFormat
 
     if with_columns is not None:
-        # TODO
-        raise ValueError("Unsupported column filter")
-        lazy_df = lazy_df[with_columns]
+        lazy_df = lazy_df.select_columns(with_columns)
 
     if predicate is not None:
-        # TODO
-        raise ValueError("Unsupported predicate")
-        lazy_df = lazy_df[predicate]
+        print("Predicate", predicate)
+        expr_ast = _to_ast(predicate)
+        adb_expression = _convert_predicate(expr_ast)
+        lazy_df = lazy_df[adb_expression]
 
     if n_rows is not None:
-        # TODO
-        raise ValueError("Unsupported predicate")
         lazy_df = lazy_df.head(n_rows)
 
-
     arrow_df = lazy_df.collect(output_format=OutputFormat.ARROW).data
-    print(arrow_df)
     result = from_arrow(arrow_df)
-    print(result)
     return result
+
+
+def _to_ast(expr: str) -> ast.expr:
+    """
+    Converts a Python string to an AST.
+
+    This will take the Python Arrow expression (as a string), and it will
+    be converted into a Python AST that can be traversed to convert it to an
+    ArcticDB ExpressionNode.
+
+    The reason to convert it to an AST is because the PyArrow expression
+    itself doesn't have any methods/properties to traverse the expression.
+    We need this to convert it into an ArcticDB expression.
+
+    Parameters
+    ----------
+    expr
+        The string expression
+
+    Returns
+    -------
+    The AST representing the Arrow expression
+    """
+    return ast.parse(expr, mode="eval").body
+
+
+@singledispatch
+def _convert_predicate(a: Any) -> Any:
+    """Walks the AST to convert the PyArrow expression to an ArcticDB expression."""
+    msg = f"Unexpected symbol: {a}"
+    raise ValueError(msg)
+
+
+@_convert_predicate.register(Constant)
+def _(a: Constant) -> Any:
+    print("Constant", a.value)
+    return a.value
+
+
+@_convert_predicate.register(Name)
+def _(a: Name) -> Any:
+    print("Name", a.id)
+    return a.id
+
+
+@_convert_predicate.register(UnaryOp)
+def _(a: UnaryOp) -> Any:
+    print("Unary", a.op, a.operand)
+    if isinstance(a.op, Invert):
+        return ~_convert_predicate(a.operand)
+    else:
+        msg = f"Unexpected UnaryOp: {a}"
+        raise TypeError(msg)
+
+
+@_convert_predicate.register(Call)
+def _(a: Call) -> Any:
+    print("Call: ", a.func, a.args)
+    args = [_convert_predicate(arg) for arg in a.args]
+    f = _convert_predicate(a.func) # TODO: Needed to uncover (expr).isin(list)
+    if f == "field":
+        print("Field: ", args, f)
+        return ExpressionNode.column_ref(args[0])
+    elif f == "scalar": # TODO: Understand this
+        return args[0]
+    elif f in _temporal_conversions: # TODO: Understand this
+        # convert from polars-native i64 to ISO8601 string
+        return _temporal_conversions[f](*args).isoformat()
+    else:
+        ref = _convert_predicate(a.func.value)  # type: ignore[attr-defined]
+        if f == "isin":
+            return ref.isin(args[0])
+        elif f == "is_null":
+            return ref.isnull()
+        elif f == "is_nan":
+            return ref.isna()
+
+    msg = f"Unknown call: {f!r}"
+    raise ValueError(msg)
+
+
+@_convert_predicate.register(Attribute)
+def _(a: Attribute) -> Any:
+    print("Attribute", a.attr, a.value)
+    return a.attr
+
+
+@_convert_predicate.register(BinOp)
+def _(a: BinOp) -> Any:
+    print("BinOp", a.op, a.left, a.right)
+    lhs = _convert_predicate(a.left)
+    rhs = _convert_predicate(a.right)
+
+    op = a.op
+    if isinstance(op, BitAnd):
+        return lhs & rhs
+    if isinstance(op, BitOr):
+        return lhs | rhs
+    else:
+        msg = f"Unknown: {lhs} {op} {rhs}"
+        raise TypeError(msg)
+
+
+@_convert_predicate.register(Compare)
+def _(a: Compare) -> Any:
+    print("Compare", a.ops, a.left, a.comparators)
+    # Compares returned by polars only ever contain a single comparison
+    op = a.ops[0]
+    left = a.left
+    right = a.comparators[0]
+    lhs = _convert_predicate(left)
+    rhs = _convert_predicate(right)
+    print("Strung op: ", str(op))
+
+    # TODO: More options. Also consider using ExpressionNode.compose(str_op)
+    if isinstance(op, Gt):
+        return lhs > rhs
+    if isinstance(op, GtE):
+        return lhs >= rhs
+    if isinstance(op, Eq):
+        return lhs == rhs
+    if isinstance(op, Lt):
+        return lhs < rhs
+    if isinstance(op, LtE):
+        return lhs <= rhs
+    else:
+        msg = f"Unknown comparison: {op}"
+        raise TypeError(msg)
+
+
+@_convert_predicate.register(List)
+def _(a: List) -> Any:
+    return [_convert_predicate(e) for e in a.elts]
--- py-polars/tests/unit/io/test_arcticdb.py
+++ py-polars/tests/unit/io/test_arcticdb.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from pathlib import Path
 import pandas as pd
 import shutil
@@ -23,7 +24,14 @@ def arctic_identifier(io_files_path: Path) -> SymbolIdentifier:
 
     ac = Arctic(uri)
     lib = ac.create_library(lib_name)
-    demo_df = pd.DataFrame({"col": [1, 2, 3]}, index=pd.date_range(start=pd.Timestamp(2025, 1, 1), periods=3))
+    num_rows = 100
+    demo_df = pd.DataFrame(
+        {
+            "int_col": range(num_rows),
+            "float_col": [2.0*i  for i in range(num_rows)],
+            "str_col": [f"str_{i}" for i in range(num_rows)]
+        },
+        index=pd.date_range(start=pd.Timestamp(2025, 1, 1), periods=num_rows))
     lib.write(symbol, demo_df)
     del lib
     del ac
@@ -37,9 +45,57 @@ class TestArcticdbScanIO:
     """Test coverage for `arcticdb` scan ops."""
 
     def test_scan_arcticdb_plain(self, arctic_identifier):
-        df = pl.scan_arcticdb(arctic_identifier)
-        assert len(df.collect()) == 3
-        assert df.collect_schema() == {
+        lf = pl.scan_arcticdb(arctic_identifier)
+        # TODO: len here and in other places is not a good test
+        assert len(lf.collect()) == 100
+        assert lf.collect_schema() == {
             "__index_level_0__": pl.Time,
-            "col": pl.Int64,
+            "int_col": pl.Int64,
+            "float_col": pl.Float64,
+            "str_col": pl.String,
         }
+
+
+    def test_scan_arcticdb_complex_processing(self, arctic_identifier):
+        # Using multiple filters and projection which will push down to arcticdb layer.
+        lf = pl.scan_arcticdb(arctic_identifier)
+        lf = lf.filter((10 <= pl.col("int_col")) & (pl.col("int_col") < 50)) # After this we will have rows between 10 and 50
+        lf = lf.filter(pl.col("float_col") <= 40.1) # After this we will have rows between 10 and 20 incl
+        lf = lf.filter(pl.col("str_col").is_in(["str_1", "str_10", "str_13", "str_17", "str_25"])) # After this we will have only rows 10, 13, 17
+        lf = lf.select(["int_col"])
+        assert len(lf.collect()) == 3
+        assert lf.collect_schema() == {
+            "int_col": pl.Int64,
+        }
+
+
+    def test_scan_arcticdb_basic_processing(self, arctic_identifier):
+        lf = pl.scan_arcticdb(arctic_identifier)
+
+        res = lf.filter(pl.col("int_col") <= 20)
+        assert len(res.collect()) == 21
+
+        res = lf.filter(pl.col("int_col") < 20)
+        assert len(res.collect()) == 20
+
+        res = lf.filter(pl.col("int_col") > 20)
+        assert len(res.collect()) == 79
+
+        res = lf.filter(pl.col("int_col") >= 20)
+        assert len(res.collect()) == 80
+
+        res = lf.filter(pl.col("int_col").is_in([10, 20, 30, 105, 40]))
+        assert len(res.collect()) == 4
+
+        res = lf.filter(pl.col("float_col").is_not_nan())
+        assert len(res.collect()) == 100
+
+        res = lf.filter(pl.col("str_col").is_not_null())
+        assert len(res.collect()) == 100
+
+        # Checks below don't do predicate pushdown.
+        res = lf.filter(pl.col("__index_level_0__") >= datetime(2025, 1, 11))
+        assert len(res.collect()) == 90
+
+        res = lf.filter(pl.col("__index_level_0__") < datetime(2025, 1, 11))
+        assert len(res.collect()) == 10
